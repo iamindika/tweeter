37adf9b084eff07187e7ac4f4916f331b26ff209
Indika Kolamba Tantrege <ikolamba2@gmail.com>
2020-11-19 19:13:24 +0000

    Create AJAX POST & GET requests

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,35 +4,10 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
 const renderTweets = function(tweetArray) {
   for (const tweetObj of tweetArray) {
     const $tweet = createTweetElement(tweetObj);
-    $('#tweets').append($tweet);
+    $('#tweets').prepend($tweet);
   }
 }
 
@@ -60,10 +35,30 @@ const createTweetElement = function(tweetData) {
         return $tweet;
 };
 
-
-// // Test / driver code (temporary)
-
 $(document).ready(function(){
-  renderTweets(data);
+
+  const loadTweets = function () {
+    $.ajax('/tweets', {method: 'GET'})
+    .then(function(moreTweets) {
+      renderTweets(moreTweets);
+    });  
+  };
+
+   loadTweets();
+
+  $('.form-tweet').submit(function(event) {
+    event.preventDefault();
+
+    $.ajax({
+      type: "POST", 
+      url: '/tweets',  
+      data: $(this).serialize() 
+    })
+    .then(function(){
+      $('#tweets').empty();
+      loadTweets();
+    });
+    
+  });
 });
 
