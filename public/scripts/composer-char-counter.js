83b17fdd9b2ee9a224420833ba236c55e94a2092
Indika Kolamba Tantrege <ikolamba2@gmail.com>
2020-11-19 00:26:34 +0000

    Implement letter counter in new tweet container
    Added reactive css style changes when character count falls under 0 and vice versa.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,21 @@
-$(document).ready(function() {
+// const { on } = require("nodemon");
 
+$(document).ready(function() {
+  
+  $('.new-tweet textarea').on('input', function() {
+    const output = $(this).parent().find('output');
+    let counter = 140 - $(this).val().length;
+  
+    if (counter < 0 && !$(output).hasClass('invalid-input')) {
+      $(output).addClass('invalid-input');
+    } else if (counter >= 0 && $(output).hasClass('invalid-input')) {
+      $(output).removeClass('invalid-input');
+    }
+    $(output).text(counter);
+  });
+  
+  // $("#tweet-text").on("keypress", function() {
+  //   console.log(($this).val());
+  // });
+  
 });
